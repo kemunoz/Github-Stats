0ef5573265ab4c39710c9ac5c4759fd1d0bbf857
Kevin Munoz <munozk341@gmail.com>
2021-03-27 13:30:52 -0700

    added avg size calculation

--- App.js
+++ App.js
@@ -11,20 +11,45 @@ app.use(helmet());
 app.use('/:username', async (req, res, next) => {
     const { username } = req.params;
     let repos = [];
-    let repoCount = 15;
+    let repoCount = 30;
     let page = 1;
-    while (repoCount === 15) {
+    let starGazerCount = 0;
+    let forkCount = 0;
+    let languageCount = {};
+    let totalSize = 0;
+    let averageSize = 0;
+    while (repoCount === 30) {
         let response = await fetch(`http://api.github.com/users/${username}/repos?per_page=30&page=${page}`);
         let json = await response.json();
         repoCount = json.length;
-        repos = [...json, ...repos];
+        repos = [...repos, ...json];
         page++;
     }
+    repos.forEach(repo => {
+        starGazerCount += repo.stargazers_count;
+        forkCount += repo.forks_count;
+        totalSize += repo.size;
+        if (repo.language) {
+            if (languageCount[repo.language]) {
+                let count = languageCount[repo.language];
+                count++;
+                languageCount[repo.language] = count;
+            } else {
+                languageCount[repo.language] = 1;
 
+            }
+        }
+    });
+
+    const sortedLanguages = Object.keys(languageCount).sort((a, b) => languageCount[a] - languageCount[b]);
     repoCount = repos.length;
+    averageSize = totalSize / repoCount;
     res.status(200).json({
-        message: "hello",
-        repoCount
+        repoCount,
+        starGazerCount,
+        forkCount,
+        languages: sortedLanguages.reverse(),
+        average_size: averageSize
     });
 })
 
